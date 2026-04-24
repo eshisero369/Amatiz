caa2a79ba0c71ee24d2475fe54692bece0d16652
eshisero369 <eshisero369@hotmail.com>
2026-04-24 14:25:59 -0500

    Update model version in main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def ia_real(mensaje):
     }
 
     data = {
-        "model": "llama3-8b-8192",
+        "model": "llama-3.1-8b-instant",
         "messages": [
             {"role": "system", "content": "Eres Ámatis IA"},
             {"role": "user", "content": mensaje}
